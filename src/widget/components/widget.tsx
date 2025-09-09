36db18af0e975e3df8b40b1a7d31d479efb6b404
Wadah <wadah@lendo.sa>
2025-09-09 17:51:41 +0300

    fix widget

--- src/widget/components/widget.tsx
+++ src/widget/components/widget.tsx
@@ -1,29 +1,28 @@
 import { useContext } from 'react';
 import { WidgetContext } from '../lib/context';
+import StockDisplay from './StockDisplay';
 
 export function Widget() {
   const { isOpen, setIsOpen } = useContext(WidgetContext);
 
   if (!isOpen) {
     return (
-      <button
-        className="widget-button"
-        onClick={() => setIsOpen(true)}
-      >
+      <button className='widget-button' onClick={() => setIsOpen(true)}>
         Open Widget
       </button>
     );
   }
 
   return (
-    <div className="widget-container">
-      <div className="widget-header">
+    <div className='widget-container'>
+      <div className='widget-header'>
         <h3>Widget Title</h3>
         <button onClick={() => setIsOpen(false)}>Close</button>
       </div>
 
-      <div className="widget-content">
+      <div className='widget-content'>
         {/* Your widget content goes here */}
+        <StockDisplay />
       </div>
     </div>
   );
